c6310b118dc2657f7670f67f155211350edeb7fb
Alice <marja1895@gmail.com>
2020-08-26 22:38:14 +0300

    adding face recognition api

--- face-recognition/src/App.js
+++ face-recognition/src/App.js
@@ -30,6 +30,24 @@ const particlesOptions = {
 	},
 };
 class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			input: "",
+		};
+	}
+
+	onInputChange = (event) => {
+		console.log(event.target.value);
+	};
+
+	onButtonSubmit = () => {
+		console.log("click");
+		app.models.predict(
+			function (response) {},
+			function (err) {}
+		);
+	};
 	render() {
 		return (
 			<div className='App'>
@@ -37,7 +55,10 @@ class App extends Component {
 				<Navigation />
 				<Logo />
 				<Rank />
-				<ImageLinkForm />
+				<ImageLinkForm
+					onInputChange={this.onInputChange}
+					onButtonSubmit={this.onButtonSubmit}
+				/>
 				{/* <FaceRecognition /> */}
 			</div>
 		);
