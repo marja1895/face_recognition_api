e9067f3a9db8e5590b6d6fbf37c1466341ec8a27
Alice <marja1895@gmail.com>
2020-09-27 13:57:20 +0300

    connecting database

--- face-recognition/src/App.js
+++ face-recognition/src/App.js
@@ -36,17 +36,39 @@ const particlesOptions = {
 		},
 	},
 };
+
+const initialState = {
+	input: "",
+	imageUrl: "",
+	box: {},
+	route: "signin",
+	isSignedIn: false,
+	user: {
+		id: "",
+		name: "",
+		email: "",
+		entries: 0,
+		joined: "",
+	},
+};
+
 class App extends Component {
 	constructor() {
 		super();
-		this.state = {
-			input: "",
-			imageUrl: "",
-			box: {},
-			route: "signin",
-			isSignedIn: false,
-		};
+		this.state = initialState;
 	}
+
+	loadUser = (data) => {
+		this.setState({
+			user: {
+				id: data.id,
+				name: data.name,
+				email: data.email,
+				entries: data.entries,
+				joined: data.joined,
+			},
+		});
+	};
 
 	calculateFaceLocation = (data) => {
 		const clarifaiFace =
@@ -74,15 +96,29 @@ class App extends Component {
 		this.setState({ imageUrl: this.state.input });
 		app.models
 			.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-			.then((response) =>
-				this.displayFaceBox(this.calculateFaceLocation(response))
-			)
+			.then((response) => {
+				if (response) {
+					fetch("http://localhost:3000/image", {
+						method: "put",
+						headers: { "Content-Type": "application/json" },
+						body: JSON.stringify({
+							id: this.state.user.id,
+						}),
+					})
+						.then((response) => response.json())
+						.then((count) => {
+							this.setState(Object.assign(this.state.user, { entries: count }));
+						})
+						.catch(console.log);
+				}
+				this.displayFaceBox(this.calculateFaceLocation(response));
+			})
 			.catch((err) => console.log(err));
 	};
 
 	onRouteChange = (route) => {
 		if (route === "signout") {
-			this.setState({ isSignedIn: false });
+			this.setState(initialState);
 		} else if (route === "home") {
 			this.setState({ isSignedIn: true });
 		}
@@ -111,7 +147,10 @@ class App extends Component {
 				) : route === "signin" ? (
 					<Signin onRouteChange={this.onRouteChange} />
 				) : (
-					<Register onRouteChange={this.onRouteChange} />
+					<Register
+						loadUser={this.loadUser}
+						onRouteChange={this.onRouteChange}
+					/>
 				)}
 			</div>
 		);
--- face_recognition_API/server.js
+++ face_recognition_API/server.js
@@ -2,103 +2,102 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
+const knex = require("knex");
+const { response } = require("express");
+const { json } = require("body-parser");
+const db = knex({
+	client: "pg",
+	connection: {
+		host: "127.0.0.1",
+		user: "postgres",
+		password: "",
+		database: "face-recognition",
+	},
+});
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-const datbase = {
-	users: [
-		{
-			id: "123",
-			name: "John",
-			email: "john@gmail.com",
-			entries: 0,
-			joined: new Date(),
-		},
-		{
-			id: "124",
-			name: "Sally",
-			email: "sally@gmail.com",
-			entries: 0,
-			joined: new Date(),
-		},
-	],
-	login: [
-		{
-			id: "987",
-			hash: "",
-			email: "john@gmail.com",
-		},
-	],
-};
-
-app.get("/", (req, res) => {
-	res.send(database.users);
-});
-
 app.post("/signin", (req, res) => {
-	// Load hash from your password DB.
-	bcrypt.compare("bacon", hash, function (err, res) {
-		// res == true
-	});
-	bcrypt.compare("veggies", hash, function (err, res) {
-		// res = false
-	});
-	if (
-		req.body.email === database.users[0].email &&
-		req.body.password === database.users[0].password
-	) {
-		res.json("success");
-	} else {
-		res.status(400).json("error logging in");
-	}
+	db.select("email", "hash")
+		.from("login")
+		.where("email", "=", req.body.email)
+		.then((data) => {
+			const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+			if (isValid) {
+				return db
+					.select("*")
+					.from("users")
+					.where("email", "=", req.body.email)
+					.then((user) => {
+						res.json(user[0]);
+					})
+					.catch((err) => res.status(400).json("unable to get user"));
+			} else {
+				res.status(400).json("wrong credentials");
+			}
+		})
+		.catch((err) => res.status(400).json("wrong credentials"));
 });
 
 app.post("/register", (req, res) => {
 	const { email, name, password } = req.body;
-	bcrypt.hash(password, null, null, function (err, hash) {
-		console.log(hash);
-	});
-	database.users.push({
-		id: "125",
-		name: name,
-		email: email,
-		passwword: password,
-		entries: 0,
-		joined: new Date(),
-	});
-	res.json(database.users[database.users.length - 1]);
+	const hash = bcrypt.hashSync(password);
+	db.transaction((trx) => {
+		trx
+			.insert({
+				hash: hash,
+				email: email,
+			})
+			.into("login")
+			.returning("email")
+			.then((loginEmail) => {
+				return trx("users")
+					.returning("*")
+					.insert({
+						email: loginEmail[0],
+						name: name,
+						joined: new Date(),
+					})
+					.then((user) => {
+						res.json(user[0]);
+					});
+			})
+			.then(trx.commit)
+			.catch(trx.rollback);
+	}).catch((err) => res.status(400).json("unable to register"));
 });
 
 app.get("/profile/:id", (req, res) => {
 	const { id } = req.params;
-	let found = false;
-	database.users.forEach((user) => {
-		if (user.id === id) {
-			found = true;
-			return res.json(user);
-		}
-	});
-	if (!found) {
-		res.status(400).json("not found");
-	}
+
+	db.select("*")
+		.from("users")
+		.where({
+			id: id,
+		})
+		.then((user) => {
+			if (user.length) {
+				res.json(user[0]);
+			} else {
+				res.status(400).json("Not found");
+			}
+		})
+		.catch((err) => res.status(400).json("Error getting user"));
 });
 
 app.put("/image", (req, res) => {
-	const { id } = req.params;
-	let found = false;
-	database.users.forEach((user) => {
-		if (user.id === id) {
-			found = true;
-			user.entries++;
-			return res.json(user.entries);
-		}
-	});
-	if (!found) {
-		res.status(400).json("not found");
-	}
+	const { id } = req.body;
+	db("users")
+		.where("id", "=", id)
+		.increment("entries", 1)
+		.returning("entries")
+		.then((entries) => {
+			res.json(entries[0]);
+		})
+		.catch((err) => res.status(400).json("unable to get entries"));
 });
 
 app.listen(3000, () => {
